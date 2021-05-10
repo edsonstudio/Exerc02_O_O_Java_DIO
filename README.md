6115769a3a8c4dcb1f9ace454769522aef53bea8
edsont8 <edsont8@hotmail.com>
2021-05-10 10:32:02 -0300

    carro class created

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Exerc02_O_O_Java_DIO
-Segundo exercício sobre Orientação à Objetos em Java
+# Exercício 02 - Java  - Orientação à Objetos
+Crie uma classe Carro com os seguintes atributos:
+- Marca: String
+- Modelo: String
+- Ano: Integer
+- Variante: String
+
+Essa classe deve garantir que Modelo, Marca e Ano sempre sejam passadas na hora de instanciar um objeto.
---
+++ src/main/java/models/Carro.java
@@ -0,0 +1,48 @@
+package models;
+
+public class Carro {
+
+    private String marca;
+    private String modelo;
+    private String variante;
+    private Integer ano;
+
+
+    public Carro(String marca, String modelo, Integer ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getVariante() {
+        return variante;
+    }
+
+    public void setVariante(String variante) {
+        this.variante = variante;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
